1a2a6d3d59ef8fcd605ef06371127651d04ec4a7
nitin sharma <157203397+nitin2005sharma@users.noreply.github.com>
2024-02-14 06:06:50 +0530

    Update
    compare two strings

---
+++ nitin7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char str1[100], str2[100];
+    int isEqual = 1; // Assume strings are equal by default
+
+   
+    printf("Enter the first string: ");
+    scanf("%s", str1);
+
+    
+    printf("Enter the second string: ");
+    scanf("%s", str2);
+
+    
+    for (int i = 0; str1[i] != '\0' || str2[i] != '\0'; i++) {
+        if (str1[i] != str2[i]) {
+            isEqual = 0; 
+            break;
+        }
+    
+    if (isEqual) {
+        printf("Strings are equal.\n");
+    } else {
+        printf("Strings are not equal.\n");
+    }
+
+    return 0;
+}
